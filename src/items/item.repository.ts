a2e8f3cc715ff3242bea73ecb24837d0fda58a2c
shun1121 <hikitanikun.ware80000@gmail.com>
2023-02-12 21:55:04 +0900

    detect the cause of the error

--- src/items/item.repository.ts
+++ src/items/item.repository.ts
@@ -25,3 +25,6 @@ export class ItemReppository extends Repository<Item> {
     return item;
   }
 }
+
+//errorの原因はデータベースにitemテーブルを追加していなかったから
+//マイグレーションファイルを作成した後、npx typeorm migration:runコマンドでデータベースにテーブルを反映させる

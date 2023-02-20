d684c7e6ffac559272fc59aad3ca37a5681a7959
shun1121 <hikitanikun.ware80000@gmail.com>
2023-02-20 23:58:37 +0900

    add login method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { User } from 'src/entities/user.entity';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { CredentialsDto } from './dto/credentials.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +12,12 @@ export class AuthController {
   //@BodyでリクエストパラメータからcreateUserDtoを受け取る
   async signup(@Body() CreateUserDto: CreateUserDto): Promise<User> {
     return await this.authService.signUp(CreateUserDto);
+  }
+
+  @Post('signin')
+  async signIn(
+    @Body() CredentialsDto: CredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return await this.authService.signIn(CredentialsDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcrypt';
+import { CredentialsDto } from './dto/credentials.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(CreateUserDto: CreateUserDto): Promise<User> {
     return await this.userRepository.createUser(CreateUserDto);
+  }
+
+  async signIn(
+    credentialsDto: CredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = credentialsDto;
+    const user = await this.userRepository.findOne({ username });
+
+    //データベースのpasswordはハッシュ化されていて、認証情報のpasswordと比較できない→bcryptを利用
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { id: user.id, username: user.username };
+      const accessToken = await this.jwtService.sign(payload); //signの引数にpayloadを渡し、トークンを作成
+      return { accessToken };
+    }
+    throw new UnauthorizedException(
+      'ユーザ名またはパスワードを確認してください。',
+    );
   }
 }

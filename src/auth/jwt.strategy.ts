de66fbfea82a80b57d7a0264cc75050b86e374a4
shun1121 <hikitanikun.ware80000@gmail.com>
2023-02-28 01:37:10 +0900

    examine jwt from request

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/entities/user.entity';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userRepository: UserRepository) {
+    // 親クラスのコンストラクターにオブジェクトを渡す。
+    super({
+      // jwtのどの部分からリクエストしているのか記述
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'secretKey123',
+    });
+  }
+
+  async validate(payload: { id: string; username: string }): Promise<User> {
+    const { id, username } = payload;
+    const user = await this.userRepository.findOne({ id, username });
+
+    if (user) {
+      return user;
+    }
+    throw new UnauthorizedException();
+  }
+}

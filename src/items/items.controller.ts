cfa05502deacdcfc6abb43f6340a7e8df89aa0be
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-01 02:55:28 +0900

    create custom decorator

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -13,6 +13,8 @@ import { CreateItemDto } from './dto/create-item.dto';
 import { Item } from '../entities/item.entity';
 import { ItemsService } from './items.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guards';
+import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { User } from 'src/entities/user.entity';
 
 @Controller('items')
 export class ItemsController {
@@ -29,7 +31,10 @@ export class ItemsController {
 
   @Post() //↓DTOを使用する場合はbodyでコレーたに引数を渡す必要はない。CreateItem型の変数を定義しておけば、その中に代入される
   @UseGuards(JwtAuthGuard)
-  async create(@Body() CreateItemDto: CreateItemDto): Promise<Item> {
+  async create(
+    @Body() CreateItemDto: CreateItemDto,
+    @GetUser() user: User,
+  ): Promise<Item> {
     //↑bodyパラメータとDTOの値が等しい場合、一つにまとめて受け取れる
     return await this.itemsService.create(CreateItemDto);
   }

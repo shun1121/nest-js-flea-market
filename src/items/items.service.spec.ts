6a1a1b18ae369b691d5d18a73adccb361d75bb87
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-13 02:53:55 +0900

    add test for findAll

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -2,7 +2,9 @@ import { Test } from '@nestjs/testing';
 import { ItemRepository } from './item.repository';
 import { ItemsService } from './items.service';
 
-const mockItemRepository = () => ({});
+const mockItemRepository = () => ({
+  find: jest.fn(), //findというmock関数を定義
+});
 // ItemsServiceはクラスなのでインスタンス化が必要
 // ItemsServiceはItemRepositoryにも依存しているからそのインスタンス化も必要
 describe('ItemServiceTest', () => {
@@ -24,5 +26,15 @@ describe('ItemServiceTest', () => {
     // moduleからItemsServiceとItemRepositoryのインスタンスを受け取る。
     itemsService = module.get<ItemsService>(ItemsService);
     itemRepository = module.get<ItemRepository>(ItemRepository);
+  });
+
+  describe('findAll', () => {
+    const expected = [];
+    it('正常系', async () => {
+      itemRepository.find.mockResolvedValue(expected);
+      const result = await itemsService.findAll();
+
+      expect(result).toEqual(expected);
+    });
   });
 });

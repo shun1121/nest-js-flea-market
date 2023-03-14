2dc1bf9e2e449f59db0095d87be6b5a57136c6d3
shun1121 <hikitanikun.ware80000@gmail.com>
2023-03-14 23:05:48 +0900

    add findById test when it's working correctly

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,10 +1,28 @@
 import { Test } from '@nestjs/testing';
+import { UserStatus } from '../auth/user-status.enum';
+import { ItemStatus } from './item-status.enum';
 import { ItemRepository } from './item.repository';
 import { ItemsService } from './items.service';
 
 const mockItemRepository = () => ({
   find: jest.fn(), //findというmock関数を定義
+  findOne: jest.fn(),
 });
+
+const mockUser1 = {
+  id: '1',
+  username: 'test1',
+  password: '1234',
+  status: UserStatus.PREMIUM,
+};
+
+const mockUser2 = {
+  id: '2',
+  username: 'test2',
+  password: '1234',
+  status: UserStatus.FREE,
+};
+
 // ItemsServiceはクラスなのでインスタンス化が必要
 // ItemsServiceはItemRepositoryにも依存しているからそのインスタンス化も必要
 describe('ItemServiceTest', () => {
@@ -34,6 +52,26 @@ describe('ItemServiceTest', () => {
       itemRepository.find.mockResolvedValue(expected);
       const result = await itemsService.findAll();
 
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('findById', () => {
+    it('正常系', async () => {
+      const expected = {
+        id: 'test-id',
+        name: 'PC',
+        price: 50000,
+        description: '',
+        status: ItemStatus.ON_SALE,
+        createdAt: '',
+        updatedAt: '',
+        userId: mockUser1.id,
+        user: mockUser1,
+      };
+
+      itemRepository.findOne.mockResolvedValue(expected);
+      const result = await itemsService.findById('test-id');
       expect(result).toEqual(expected);
     });
   });
